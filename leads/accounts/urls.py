0bdfdb0bc0b7c73caa4586396c2350809e48f21f
nealwaga <waganeal@gmail.com>
2023-04-17 10:15:59 +0300

    created url endpoints

---
+++ leads/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .api import SignUpAPI, SignInAPI, MainUser
+from knox import views as knox_views
+
+# Create here
+urlpatterns = [
+    path('api/auth/', include('knox.urls')),
+    path('api/auth/register', SignUpAPI.as_view()),
+    path('api/auth/login', SignInAPI.as_view()),
+    path('api/auth/user', MainUser.as_view()),
+    path('api/auth/logout',knox_views.LogoutView.as_view(), name="knox-logout"),
+]
